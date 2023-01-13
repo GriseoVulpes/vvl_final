9334f2b3b6d91e073d3ee2ba269f15628b5dea81
GriseoVulpes <vashsosed04@gmail.com>
2023-01-13 22:01:14 +0200

    DBV0

--- src/main/java/vvl/samishamoon/Main.java
+++ src/main/java/vvl/samishamoon/Main.java
@@ -1,26 +1,19 @@
 package vvl.samishamoon;
 
-import vvl.samishamoon.coffe_shop.Dishe;
+import vvl.samishamoon.sql.SQL_functions;
+import vvl.samishamoon.users.*;
 
-import java.sql.*;
-import java.util.Arrays;
-import java.util.Properties;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        Dishe d = new Dishe("s","s", 5.55, 5.543,32,2,1);
-        System.out.println(d);
-        String url = "jdbc:postgresql://localhost:5432/COFFEESHOP";
-        Properties props = new Properties();
-        props.setProperty("user", "ADMIN");
-        props.setProperty("password", "123456789");
+        SQL_functions g = new SQL_functions();
 
+        g.testDB();
+        ArrayList<Admin> as = g.getAdmins();
+        System.out.println(as);
 
-        try (Connection conn = DriverManager.getConnection(url, props)) {
-            System.out.println(conn.getMetaData().getDatabaseProductVersion());
-        } catch(SQLException e) {
-            System.out.println("Error connecting to database " + Arrays.toString(e.getStackTrace()));
-        }
+        Admin a1 = g.getAdmin(2);
+        System.out.println(a1);
     }
 }
